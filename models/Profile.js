fa688ec2d5a7f18bea907f893789fe6b230cd739
Abubakar Shehbaz <50992500+bkarshehbaz@users.noreply.github.com>
2021-12-03 09:19:39 +0500

    everything is working

---
+++ models/Profile.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+
+  name: {
+    type: String
+  },
+  bio: {
+    type: String
+  },
+
+  sos: {
+    type: String
+  },
+
+  jobtitle: {
+    type: String
+  },
+  contactnumber: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  address: {
+    type: String
+  },
+  website: {
+    type: String,
+    required: true
+  },
+
+  instagram: {
+    type: String,
+    required: true
+  },
+  linkedin: {
+    type: String,
+    required: true
+  },
+  facebook: {
+    type: String,
+    required: true
+  },
+  whatsapp: {
+    type: String,
+    required: true
+  },
+  behance: {
+    type: String,
+    required: true
+  },
+  twitter: {
+    type: String,
+    required: true
+  },
+  dp: {
+    type: String,
+    required: true
+  },
+  serialnumber: {
+    type: String,
+    required: true
+  },
+
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('profile', ProfileSchema);
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const path = require("path");
 const bodyParser = require("body-parser");
@@ -7,14 +8,17 @@ const app = express();
 
 // Connect Database
 
+// Connect Database
+connectDB();
+
 // Init Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 // Define Routes
-// app.use("/api/users", require("./routes/api/users"));
-// app.use("/api/auth", require("./routes/api/auth"));
-// app.use("/api/profile", require("./routes/api/profile"));
-// app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 // // Serve static assets in production
 // if (process.env.NODE_ENV === "production") {
